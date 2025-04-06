5d371b502496d2dc972937cf60a2215884ae7853
yuriiup <kaktus20110@yandex.com>
2025-04-06 21:00:38 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,14 +3,27 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def mars1():
-    return "Миссия Колонизация Марса"
-
-
-@app.route('/index')
-def index():
-    return 'И на Марсе будут яблони цвести!'
+@app.route('/promotion_image')
+def image_mars():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <div>
+                      <img src="/static/img/MARS.png" alt="здесь должна была быть картинка, но не нашлась">
+                    </div>
+                    <div1>Вот она какая, красная планета</div1>
+                  </body>
+                </html>'''
 
 
 if __name__ == '__main__':
